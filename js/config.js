a467c9ed6d1fb5c1308048a0fbe7aeea2efceeb8
hesham <do.everything.hgh@gmail.com>
2026-01-03 22:01:23 +0200

    7777777777777

--- js/config.js
+++ js/config.js
@@ -15,7 +15,7 @@
  * @type {string}
  * @var
  */
-window.VERCEL_URL = "https://suez-bazaar.vercel.app";
+window.VERCEL_URL = "https://suezbazaar.vercel.app";
 
 /**
  * @description List of domain names allowed to use the Vercel URL.
@@ -28,7 +28,7 @@ window.allowedHosts = [
   "suez-bazaar.pages.dev",
   "appassets.androidplatform.net",
   "doeverythinghgh-boop.github.io",
-  "suez-bazaar.vercel.app",
+  "suezbazaar.vercel.app",
 ];
 
 // ✅ Improvement: Simplify baseURL determination logic using an array.
